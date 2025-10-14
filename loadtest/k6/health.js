e932b72f74dcb6a35fb6def772071760ff251331
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-10-14 18:28:38 +0200

    Updated health.js

--- loadtest/k6/health.js
+++ loadtest/k6/health.js
@@ -1,11 +1,51 @@
-import http from "k6/http";
-import { sleep, check } from "k6";
-import { Counter } from "k6/metrics";
-import { readBaseline, storeResult } from "./helper/compare-result.js";
+let package_loader = false; /* false if NodeJS uses CommonJS, true if NodeJS uses ECMAScript */
 
-export const requests = new Counter('http_reqs');
+try {
 
-const baseline = readBaseline("health");
+    require.main
+    
+} catch(err) {
+
+    package_loader = true;
+    
+};
+
+var dependancies = {};
+
+if (package_loader === false) {
+
+    let http = require("k6/http"),
+        { sleep, check } = require("k6"),
+        { Counter } = require("k6/metrics"),
+        { readBaseline, storeResult } = require("./helper/compare-result.js");
+
+    dependancies["http"] = http;
+    dependancies["sleep"] = sleep;
+    dependancies["check"] = check;
+    dependancies["Counter"] = Counter;
+    dependancies["readBaseline"] = readBaseline;
+    dependancies["storeResult"] = storeResult;
+    
+};
+if (package_loader === true) {
+    
+    import http from "k6/http";
+    import { sleep, check } from "k6";
+    import { Counter } from "k6/metrics";
+    import { readBaseline, storeResult } from "./helper/compare-result.js";
+    
+    dependancies["http"] = http;
+    dependancies["sleep"] = sleep;
+    dependancies["check"] = check;
+    dependancies["Counter"] = Counter;
+    dependancies["readBaseline"] = readBaseline;
+    dependancies["storeResult"] = storeResult;
+
+};
+
+export const requests = new dependancies["Counter"]('http_reqs');
+
+const baseline = dependancies["readBaseline"]("health");
 
 export const options = {
     stages: [
@@ -19,12 +59,16 @@ export const options = {
 };
 
 export default function () {
-    const res = http.get("http://router-server:8080/health");
-    check(res, {
+    
+    const res = dependancies["http"].get("http://router-server:8080/health");
+    dependanciees["check"](res, {
         "health status 200": (r) => r.status === 200,
     });
-}
+    
+};
 
 export function handleSummary(data) {
-    return storeResult("health", baseline, data)
-}
+    
+    return dependancies["storeResult"]("health", baseline, data);
+    
+};
