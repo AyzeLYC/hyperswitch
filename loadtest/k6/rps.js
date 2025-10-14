690f2e5af160b5f6dd36ec8971a4fdac04f2e223
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-10-14 17:57:09 +0200

    Updated rps.js

--- loadtest/k6/rps.js
+++ loadtest/k6/rps.js
@@ -1,13 +1,55 @@
-import { group } from 'k6';
-import { Counter } from "k6/metrics";
-import { readBaseline, storeResult } from "./helper/compare-result.js";
-import { setup_merchant_apikey } from "./helper/setup.js";
-import paymentCreateAndConfirmFunc from './payment-create-and-confirm.js';
-import paymentConfirmFunc from './payment-confirm.js';
+let package_loader = false; /* false if NodeJS is using CommonJS, true if NodeJS is using ECMAScript */
 
-export const requests = new Counter("http_reqs");
+try {
 
-const baseline = readBaseline("rps");
+    require.main;
+    
+} catch(err) {
+
+    package_loader = true;
+    
+};
+
+var dependancies = {};
+
+if (package_loader === false) {
+
+    let { group } = require("k6"),
+        { Counter } = require("k6/metrics"),
+        { readBaseline, storeResult } = require("./helper/compare-result.js"),
+        { setup_merchant_apikey } = require("./helper/setup.js"),
+        paymentCreateAndConfirmFunc = require("./payment-create-and-confirm.js"),
+        paymentConfirmFunc = require("./payment-confirm.js");
+    
+    dependancies["group"] = group;
+    dependancies["Counter"] = Counter;
+    dependancies["readBaseline"] = readBaseline;
+    dependancies["storeResult"] = storeResult;
+    dependancies["setup_merchant_apikey"] = setup_merchant_apikey;
+    dependancies["paymentCreateAndConfirmFunc"] = paymentCreateAndConfirmFunc;
+    
+};
+if (package_loader === true) {
+    
+    import { group } from 'k6';
+    import { Counter } from "k6/metrics";
+    import { readBaseline, storeResult } from "./helper/compare-result.js";
+    import { setup_merchant_apikey } from "./helper/setup.js";
+    import paymentCreateAndConfirmFunc from './payment-create-and-confirm.js';
+    import paymentConfirmFunc from './payment-confirm.js';
+    
+    dependancies["group"] = group;
+    dependancies["Counter"] = Counter;
+    dependancies["readBaseline"] = readBaseline;
+    dependancies["storeResult"] = storeResult;
+    dependancies["setup_merchant_apikey"] = setup_merchant_apikey;
+    dependancies["paymentCreateAndConfirmFunc"] = paymentCreateAndConfirmFunc;
+    
+};
+
+export const requests = new dependancies["Counter"]("http_reqs");
+
+const baseline = dependancies["readBaseline"]("rps");
 
 export const options = {
     scenarios: {
@@ -21,14 +63,28 @@ export const options = {
 };
 
 export function setup() {
-    return setup_merchant_apikey()
-}
+    
+    return dependancies["setup_merchant_apikey"]();
+    
+};
 
 export default function (data) {
-    group("create payment and confirm", function() { paymentCreateAndConfirmFunc(data) });
-    group("create confirmed payment", function() { paymentConfirmFunc(data) });
-}
+    
+    dependancies["group"]("create payment and confirm", function() {
+        
+        dependancies["paymentCreateAndConfirmFunc"](data);
+        
+    });
+    dependancies["group"]("create confirmed payment", function() {
+        
+        dependancies["paymentConfirmFunc"](data);
+        
+    });
+    
+};
 
 export function handleSummary(data) {
-    return storeResult("rps", baseline, data)
-}
+    
+    return dependancies["storeResult"]("rps", baseline, data);
+    
+};
